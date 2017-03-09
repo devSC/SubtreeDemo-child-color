c3b96cc7f23441698068c4479868cfffebfd8f85
Wilson Yuan <yuansc@nihao.com>
2017-03-09 17:46:54 +0800

    add [subtree sub color]

---
+++ SubtreeSubColor.m
@@ -0,0 +1,17 @@
+//
+//  SubtreeSubColor.m
+//  SubtreeDemo
+//
+//  Created by Wilson Yuan on 2017/3/9.
+//  Copyright © 2017年 Wilson Yuan. All rights reserved.
+//
+
+#import "SubtreeSubColor.h"
+
+@implementation SubtreeSubColor
+
++ (void)hello {
+    NSLog(@"%s", __FUNCTION__);
+}
+
+@end
