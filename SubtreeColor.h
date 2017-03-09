9af1893cae3b479234c68d2c161cbc9f757180f4
Wilson Yuan <yuansc@nihao.com>
2017-03-09 17:28:53 +0800

    initial file

---
+++ SubtreeColor.h
@@ -0,0 +1,15 @@
+//
+//  SubtreeColor.h
+//  SubtreeDemo
+//
+//  Created by Wilson Yuan on 2017/3/9.
+//  Copyright © 2017年 Wilson Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubtreeColor : UIColor
+
++ (UIColor *)subRedColor;
+
+@end
---
+++ SubtreeColor.m
@@ -0,0 +1,17 @@
+//
+//  SubtreeColor.m
+//  SubtreeDemo
+//
+//  Created by Wilson Yuan on 2017/3/9.
+//  Copyright © 2017年 Wilson Yuan. All rights reserved.
+//
+
+#import "SubtreeColor.h"
+
+@implementation SubtreeColor
+
++ (UIColor *)subRedColor {
+    return [[self redColor] colorWithAlphaComponent:0.6];
+}
+
+@end
