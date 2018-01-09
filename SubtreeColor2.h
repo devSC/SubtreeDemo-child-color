9dd52a73771af1443431322b55d0eb9e4d5e3848
Wilson <xiaochong2154@163.com>
2018-01-09 19:57:22 +0800

    update subtree color2

---
+++ SubtreeColor2.h
@@ -0,0 +1,13 @@
+//
+//  SubtreeColor2.h
+//  SubtreeDemo
+//
+//  Created by Wilson Yuan on 2018/1/9.
+//  Copyright © 2018年 Wilson Yuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubtreeColor2 : NSObject
+
+@end
---
+++ SubtreeColor2.m
@@ -0,0 +1,13 @@
+//
+//  SubtreeColor2.m
+//  SubtreeDemo
+//
+//  Created by Wilson Yuan on 2018/1/9.
+//  Copyright © 2018年 Wilson Yuan. All rights reserved.
+//
+
+#import "SubtreeColor2.h"
+
+@implementation SubtreeColor2
+
+@end
